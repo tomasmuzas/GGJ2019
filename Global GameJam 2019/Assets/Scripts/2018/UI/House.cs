ad43e60af2ed529139f185b75976ce82d60e7e2b
Tomas Mūžas <root@DESKTOP-AB3PSFR.localdomain>
2019-01-27 01:12:17 +0200

    Add basic house logic

--- Global GameJam 2019/Assets/Scripts/2018/UI/House.cs
+++ Global GameJam 2019/Assets/Scripts/2018/UI/House.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts._2018.UI
 {
@@ -6,11 +9,36 @@ namespace Assets.Scripts._2018.UI
     {
         public Sprite[] States;
 
-        public HealthManager HealthManager;
+        private HealthManager HealthManager;
 
         public void Start()
         {
             HealthManager = GetComponent<HealthManager>();
+        }
+
+        public void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "DamageDealer")
+            {
+                HealthManager.DealDamage(HealthObjectType.Health, 1);
+                GetComponent<SpriteRenderer>().sprite = States[GetCurrentSprite()];
+                Destroy(collision.gameObject);
+            }
+        }
+
+        public int GetCurrentSprite()
+        {
+            if (States.Length == 0)
+            {
+                throw new ArgumentException("There must be at least one state");
+            }
+
+            var percentagePerSprite = 100 / States.Length;
+
+            var index = (int)HealthManager.GetPercentage(HealthObjectType.Health) / (int)percentagePerSprite;
+
+            return index;
+
         }
     }
 }
