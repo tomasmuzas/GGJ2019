6244d880842d882f5edcafd1171034c88bf63a75
Povilas Galčius <povilas.galcius@devbridge.com>
2019-01-27 01:41:59 +0200

    Added intro scene

---
+++ Global GameJam 2019/Assets/Dissapear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dissapear : MonoBehaviour
+{
+    public GameObject[] Sprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var transp = 5 - Time.timeSinceLevelLoad;
+        foreach (var sprite in Sprites)
+        {
+            var image = sprite.GetComponent<Image>();
+            var tempColor = image.color;
+            tempColor.a = transp;
+            image.color = tempColor;
+        }
+    }
+}
---
+++ Global GameJam 2019/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+public class ChangeScene : MonoBehaviour
+{
+    #region FIELDS
+    public Image fadeOutUIImage;
+    public float fadeSpeed = 0.3f;
+    public enum FadeDirection
+    {
+        In, //Alpha = 1
+        Out // Alpha = 0
+    }
+    #endregion
+    #region MONOBHEAVIOR
+    public bool fading = false;
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad > 3 && !fading)
+        {
+            fading = true;
+            StartCoroutine(FadeAndLoadScene(FadeDirection.Out));
+        }
+    }
+    void OnEnable()
+    {
+        //StartCoroutine(Fade(FadeDirection.Out));
+
+    }
+    public void Skip()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    #endregion
+    #region FADE
+    private IEnumerator Fade(FadeDirection fadeDirection)
+    {
+        float alpha = (fadeDirection == FadeDirection.Out) ? 1 : 0;
+        float fadeEndValue = (fadeDirection == FadeDirection.Out) ? 0 : 1;
+        if (fadeDirection == FadeDirection.Out)
+        {
+            while (alpha >= fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+            fadeOutUIImage.enabled = false;
+        }
+        else
+        {
+            fadeOutUIImage.enabled = true;
+            while (alpha <= fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+        }
+    }
+    #endregion
+    #region HELPERS
+    public IEnumerator FadeAndLoadScene(FadeDirection fadeDirection)
+    {
+        yield return Fade(fadeDirection);
+        Debug.Log("scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    private void SetColorImage(ref float alpha, FadeDirection fadeDirection)
+    {
+        fadeOutUIImage.color = new Color(fadeOutUIImage.color.r, fadeOutUIImage.color.g, fadeOutUIImage.color.b, alpha);
+        alpha += Time.deltaTime * (1.0f / fadeSpeed) * ((fadeDirection == FadeDirection.Out) ? -1 : 1);
+    }
+    #endregion
+}
