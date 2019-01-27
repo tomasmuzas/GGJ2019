9e143c2f0e3477ebd094210196135d6c063885cf
Povilas Galčius <povilas.galcius@devbridge.com>
2019-01-27 04:39:51 +0200

    Back button and new backgound

---
+++ Global GameJam 2019/Assets/GameOverWin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverWin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            //PlayGames.AddScoreToLeaderBoard(GPGSIds.leaderboard_score, score);
+            SceneManager.LoadScene(0);
+            Time.timeScale = 1F;
+        }
+    }
+}
