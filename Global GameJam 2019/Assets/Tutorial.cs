621895eb6c92505119a1070bfcd7ce8cc9693f9b
Povilas Galčius <povilas.galcius@devbridge.com>
2019-01-31 00:06:59 +0200

    Added tutorial

---
+++ Global GameJam 2019/Assets/Tutorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject[] TutorialImages;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("Watched Tutorial", 0) > 0)
+        {
+            foreach (var img in TutorialImages)
+            {
+                Destroy(img);
+            }
+        }
+    }
+
+    public void HideTutorial()
+    {
+        PlayerPrefs.SetInt("Watched Tutorial", 1);
+        foreach (var img in TutorialImages)
+        {
+            Destroy(img);
+        }
+    }
+}
